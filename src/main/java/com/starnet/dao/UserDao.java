530a2b09028751253897fad6f3ae1eae043338dd
crie12138 <30739570+crie12138@users.noreply.github.com>
2020-01-10 16:10:12 +0800

    Create UserDao.java

---
+++ src/main/java/com/starnet/dao/UserDao.java
@@ -0,0 +1,34 @@
+package com.starnet.dao;
+
+import com.starnet.bean.User;
+import com.starnet.bean.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserDao {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    List<User> selectAll();
+
+    int deleteByKey(int id);
+}
